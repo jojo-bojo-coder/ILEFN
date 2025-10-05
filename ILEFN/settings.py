dddabbe5db2b7ef688b3f3d35c3c22ba45f6f5c9
jojo cabanojo <receptionist402@gmail.com>
2025-10-05 21:53:32 +0300

    Update settings.py

--- ILEFN/settings.py
+++ ILEFN/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-65nd4@+*y86f6&rh1iblt$w^)8dae0@f*n5+-x!c3s9^!ji-t&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
